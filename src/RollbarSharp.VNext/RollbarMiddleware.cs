4fcd7610f70e2303caa15def085abd8d2ed4ff24
James Dibble <james@jdibble.co.uk>
2015-06-22 15:07:14 +0100

    Adding middleware component

---
+++ src/RollbarSharp.VNext/RollbarMiddleware.cs
@@ -0,0 +1,53 @@
+﻿namespace RollbarSharp.VNext
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+
+    public class RollbarMiddleware
+    {
+        private readonly Lazy<RollbarClient> _rollbar;
+        private readonly RequestDelegate _next;
+
+        public RollbarMiddleware(RequestDelegate next, Configuration config)
+        {
+            this._rollbar = new Lazy<RollbarClient>(() => BuildClientFromConfig(config));
+
+            this._next = next;
+        }
+
+        public RollbarMiddleware(RequestDelegate next, string accessToken)
+        {
+            this._rollbar = new Lazy<RollbarClient>(() => BuildClient(accessToken));
+
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await this._next(context);
+            }
+            catch (Exception ex)
+            {
+                Task.WaitAll(this._rollbar.Value.SendException(ex, context));
+
+                throw;
+            }
+        }
+
+        private static RollbarClient BuildClient(string accessToken)
+        {
+            return BuildClientFromConfig(new Configuration(accessToken));
+        }
+
+        private static RollbarClient BuildClientFromConfig(Configuration config)
+        {
+            var client = new RollbarClient(config);
+
+            return client;
+        }
+    }
+}
