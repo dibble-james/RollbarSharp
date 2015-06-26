2c1108a258fe845d7a3133c8f025f90db58028d8
James Dibble <james@jdibble.co.uk>
2015-06-26 16:11:51 +0100

    Adding revised PersonModelBuilder

---
+++ src/RollbarSharp.VNext/Builders/PersonModelBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace RollbarSharp.Builders
+{
+    using Microsoft.AspNet.Http;
+    using RollbarSharp.Serialization;
+
+    public class PersonModelBuilder
+    {
+        /// <summary>
+        /// Use the current identity from the <see cref="HttpContext"/>.
+        /// Email address won't be set.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static PersonModel CreateFromHttpRequest(HttpContext request)
+        {
+            var username = request.User.Identity.IsAuthenticated ? request.User.Identity.Name : string.Empty;
+
+            return new PersonModel { Id = username, Username = username };
+        }
+    }
+}
